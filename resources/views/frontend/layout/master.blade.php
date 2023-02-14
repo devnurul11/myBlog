b5946289e699484d1a9d03da291f9551cc043ca5
Nurul Islam <devnurul11@gmail.com>
2023-02-15 03:10:19 +0600

    add scripts

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -24,19 +24,12 @@
 
     <footer>
    @include('frontend/includes/footer')
+   
     </footer>
     
 <!-- Javascripts -->
-<script src="{{ asset('frontend/js/jquery-2.1.4.min.js') }}"></script><!-- jQuery library -->
-<script src="{{ asset('frontend/js/smoothscroll.js') }}"></script>
-<script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script> 
-<script src="{{ asset('frontend/js/wow.min.js') }}"></script>
-<script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('frontend/js/typed.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('frontend/js/main.js') }}"></script>
-    
+
+    @include('frontend/includes/footer')
     
  
 </body>
