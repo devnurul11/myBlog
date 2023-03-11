b6b3666bf1792fa44a3b5edbff51bf9c88fce779
Nurul Islam <devnurul11@gmail.com>
2023-03-12 01:51:27 +0600

    add tag to posst

--- resources/views/backend/modules/category/index.blade.php
+++ resources/views/backend/modules/category/index.blade.php
@@ -59,7 +59,11 @@
                                             <a href="{{ route('category.edit', $category->id) }}"><button
                                                     class="btn btn-warning btn-sm mx-1"><i
                                                         class="fa-solid fa-edit"></i></button></a>
-                                            {!! Form::open(['method' => 'delete','id'=>'deleteForm-'.$category->id, 'route' => ['category.destroy', $category->id]]) !!}
+                                            {!! Form::open([
+                                                'method' => 'delete',
+                                                'id' => 'deleteForm-' . $category->id,
+                                                'route' => ['category.destroy', $category->id],
+                                            ]) !!}
                                             {!! Form::button('<i class="fa-solid fa-trash"></i>', [
                                                 'class' => 'btn btn-danger btn-sm delete',
                                                 'type' => 'button',
@@ -97,19 +101,19 @@
                 let id = $(this).attr('data-id')
 
                 Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then((result) => {
-  if (result.isConfirmed) {
-    $('#deleteForm-'+id).submit();
-  }
-})
-                
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $('#deleteForm-' + id).submit();
+                    }
+                })
+
 
             })
         </script>
